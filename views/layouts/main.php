03db5e0f02ed12e10ecaff4e5933edc97a6b4881
stefandurlesteanu <stefan.durlesteanu@gmail.com>
2020-11-02 21:38:33 +0200

    refactored + added Controllers

---
+++ views/layouts/main.php
@@ -0,0 +1,52 @@
+<?php ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- CSS only -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+</head>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/contact">Contact</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/login">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/register">Register</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
+<body>
+
+<div class="container">
+    {{content}}
+</div>
+
+<!-- JavaScript Bundle with Popper.js -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.bundle.min.js" integrity="sha384-BOsAfwzjNJHrJ8cZidOg56tcQWfp6y72vEJ8xQ9w6Quywb24iOsW913URv1IS4GD" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ views/register.php
@@ -0,0 +1,36 @@
+<br>
+<h1>Sign Up</h1>
+<br>
+<form action="" method="post">
+    <div class="row">
+        <div class="col">
+            <div class="mb-3">
+                <label>Firstname</label>
+                <input type="text" class="form-control" name="firstname">
+            </div>
+        </div>
+        <div class="col">
+            <div class="mb-3">
+                <label>Lastname</label>
+                <input type="text" class="form-control" name="lastname">
+            </div>
+        </div>
+    </div>
+
+
+    <div class="mb-3">
+        <label>Email</label>
+        <input type="text" class="form-control" name="email">
+    </div>
+    <div class="mb-3">
+        <label>Password</label>
+        <input type="password" class="form-control" name="password">
+    </div>
+    <div class="mb-3">
+        <label>Confirm password</label>
+        <input type="password" class="form-control" name="confirmPassword">
+    </div>
+
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
